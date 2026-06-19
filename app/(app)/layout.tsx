730321762351b631309fea25359a388454e18867
sivantelem-commits <sivantelem@gmail.com>
2026-06-19 14:32:43 +0300

    Create layout.tsx

---
+++ app/(app)/layout.tsx
@@ -0,0 +1,14 @@
+import Sidebar from '@/components/layout/Sidebar';
+
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <main className="flex-1 mr-60 min-h-screen">
+        <div className="max-w-6xl mx-auto px-6 py-8">
+          {children}
+        </div>
+      </main>
+    </div>
+  );
+}
